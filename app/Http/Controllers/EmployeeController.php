b32aa44e7cbdc8390040ecb2ce15399462bc1ff9
baguscs <cahyob59@gmail.com>
2023-05-17 20:52:52 +0700

    use query builder for crud

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Validator;
 
 class EmployeeController extends Controller
@@ -14,7 +15,17 @@ class EmployeeController extends Controller
     {
         $pageTitle = 'Employee List';
 
-        return view('employee.index', ['pageTitle' => $pageTitle]);
+        // $employees = DB::select(
+        //     'select *, employees.id as employee_id, positions.name as position_name
+        //     from employees
+        //     left join positions on employees.position_id = positions.id'
+        // );
+
+        $employees = DB::table('employees')
+                    ->leftJoin('positions', 'employees.position_id', '=', 'positions.id')
+                    ->get();
+
+        return view('employee.index', compact('pageTitle', 'employees'));
     }
 
     /**
@@ -23,8 +34,9 @@ class EmployeeController extends Controller
     public function create()
     {
         $pageTitle = 'Create Employee';
+        $positions = DB::table('positions')->get();
 
-        return view('employee.create', compact('pageTitle'));
+        return view('employee.create', compact('pageTitle', 'positions'));
     }
 
     /**
@@ -49,6 +61,14 @@ class EmployeeController extends Controller
             return redirect()->back()->withErrors($validator)->withInput();
         }
 
+         DB::table('employees')->insert([
+            'firstname' => $request->firstName,
+            'lastname' => $request->lastName,
+            'email' => $request->email,
+            'age' => $request->age,
+            'position_id' => $request->position,
+        ]);
+
         return $request->all();
 
     }
@@ -58,7 +78,21 @@ class EmployeeController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $pageTitle = 'Employee Detail';
+
+        // $employee = collect(DB::select('
+        //     select *, employees.id as employee_id, positions.name as position_name
+        //     from employees
+        //     left join positions on employees.position_id = positions.id
+        //     where employees.id = ?',
+        //     [$id])
+        // )->first();
+
+        $employee = DB::table('employees')
+            ->leftJoin('positions', 'employees.position_id', '=', 'positions.id')
+            ->where('employees.id', $id)->first();
+
+        return view('employee.show', compact('pageTitle', 'employee'));
     }
 
     /**
@@ -66,7 +100,15 @@ class EmployeeController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $pageTitle = ' Edit Employee';
+
+        $employee = DB::table('employees')
+            ->leftJoin('positions', 'employees.position_id', '=', 'positions.id')
+            ->where('employees.id', $id)->first();
+
+        $positions = DB::table('positions')->get();
+
+        return view('employee.edit', compact('pageTitle', 'employee', 'positions'));
     }
 
     /**
@@ -74,7 +116,50 @@ class EmployeeController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $messages = [
+            'required' => ':Attribute harus diisi.',
+            'email' => 'Isi :attribute dengan format yang benar',
+            'numeric' => 'Isi :attribute dengan angka'
+        ];
+
+        $validator = Validator::make($request->all(), [
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required|email',
+            'age' => 'required|numeric',
+        ], $messages);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        $getEmployee = DB::table('employees')
+            ->leftJoin('positions', 'employees.position_id', '=', 'positions.id')
+            ->where('employees.id', $id)->first();
+
+        if ($getEmployee->email == $request->email) {
+            DB::table('employees')
+            ->where('id', $id)
+            ->update([
+                'firstname' => $request->firstName,
+                'lastname' => $request->lastName,
+                'age' => $request->age,
+                'position_id' => $request->position,
+            ]);
+        }
+        else{
+            DB::table('employees')
+            ->where('id', $id)
+            ->update([
+                'firstname' => $request->firstName,
+                'lastname' => $request->lastName,
+                'email' => $request->email,
+                'age' => $request->age,
+                'position_id' => $request->position,
+            ]);
+        }
+
+        return redirect()->route('employees.index')->with('success', 'Berhasil Mengupdate Data');
     }
 
     /**
@@ -82,6 +167,10 @@ class EmployeeController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        DB::table('employees')
+            ->where('id', $id)
+            ->delete();
+
+        return redirect()->route('employees.index');
     }
 }
--- resources/views/employee/edit.blade.php
+++ resources/views/employee/edit.blade.php
@@ -32,8 +32,9 @@
     </nav>
 
     <div class="container-sm mt-5">
-        <form action="{{ route('employees.store') }}" method="POST">
+        <form action="{{ route('employees.update', $employee->id) }}" method="POST">
             @csrf
+            @method('PUT')
             <div class="row justify-content-center">
                 <div class="p-5 bg-light rounded-3 border col-xl-6">
 
@@ -48,13 +49,13 @@
 
                     <div class="mb-3 text-center">
                         <i class="bi-person-circle fs-1"></i>
-                        <h4>Create Employee</h4>
+                        <h4>{{ $pageTitle }}</h4>
                     </div>
                     <hr>
                     <div class="row">
                         <div class="col-md-6 mb-3">
                             <label for="firstName" class="form-label">First Name</label>
-                            <input class="form-control @error('firstName') is-invalid @enderror" type="text" name="firstName" id="firstName" value="{{ old('firstName') }}" aria-describedby="firstNameFeedBack" placeholder="Enter First Name">
+                            <input class="form-control @error('firstName') is-invalid @enderror" type="text" name="firstName" id="firstName" value="{{ $employee->firstname }}" aria-describedby="firstNameFeedBack" placeholder="Enter First Name">
                             @error('firstName')
                                 <div id="firstNameFeedBack" class="invalid-feedback">
                                     {{ $message }}
@@ -63,7 +64,7 @@
                         </div>
                         <div class="col-md-6 mb-3">
                             <label for="lastName" class="form-label">Last Name</label>
-                            <input class="form-control @error('lastName') is-invalid @enderror" type="text" name="lastName" id="lastName" value="{{ old('lastName') }}" aria-describedby="LastNameFeedBack" placeholder="Enter Last Name">
+                            <input class="form-control @error('lastName') is-invalid @enderror" type="text" name="lastName" id="lastName" value="{{ $employee->lastname }}" aria-describedby="LastNameFeedBack" placeholder="Enter Last Name">
                             @error('lastName')
                                 <div id="lastNameFeedBack" class="invalid-feedback">
                                     {{ $message }}
@@ -72,7 +73,7 @@
                         </div>
                         <div class="col-md-6 mb-3">
                             <label for="email" class="form-label">Email</label>
-                            <input class="form-control @error('email') is-invalid @enderror" type="text" name="email" id="email" value="{{ old('email') }}" aria-describedby="emailFeedBack" placeholder="Enter Email">
+                            <input class="form-control @error('email') is-invalid @enderror" type="text" name="email" id="email" value="{{ $employee->email }}" aria-describedby="emailFeedBack" placeholder="Enter Email">
                             @error('email')
                                 <div id="emailFeedBack" class="invalid-feedback">
                                     {{ $message }}
@@ -81,7 +82,7 @@
                         </div>
                         <div class="col-md-6 mb-3">
                             <label for="age" class="form-label">Age</label>
-                            <input class="form-control @error('age') is-invalid @enderror" type="text" name="age" id="age" value="{{ old('age') }}" aria-describedby="ageFeedBack" placeholder="Enter Age">
+                            <input class="form-control @error('age') is-invalid @enderror" type="text" name="age" id="age" value="{{ $employee->age }}" aria-describedby="ageFeedBack" placeholder="Enter Age">
                             @error('age')
                                 <div id="ageFeedBack" class="invalid-feedback">
                                     {{ $message }}
@@ -92,7 +93,7 @@
                             <label for="position" class="form-label">Position</label>
                             <select name="position" id="position" class="form-select">
                                 @foreach ($positions as $position)
-                                    <option value="{{ $position->id }}" {{ old('position') == $position->id ? 'selected' : '' }}>{{ $position->code.' - '.$position->name }}</option>
+                                    <option value="{{ $position->id }}" {{ $employee->position_id == $position->id ? 'selected' : '' }}>{{ $position->code.' - '.$position->name }}</option>
                                 @endforeach
                             </select>
                             @error('position')
